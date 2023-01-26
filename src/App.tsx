abc958f58c807b120ccf14c56926775830e0ff4c
VladimirKasharevsky <kashar015@ambler.ru>
2023-01-26 22:32:36 +0300

    hello react app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './sber-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="sber-logo.png" alt="logo" width="200" height="60"/>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
